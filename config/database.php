cda6d285d2f3ed1de45877d0816d4e4c256b95c6
dameengineer <coupderaven@gmail.com>
2025-12-03 11:39:29 +0700

    Enable SSL for TiDB Database

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            
-            // TAMBAHKAN BAGIAN OPSI SSL INI:
+            // --- BAGIAN INI KUNCINYA (SSL) ---
             'options' => extension_loaded('pdo_mysql') ? array_filter([
+                // Jika di Vercel (Production), pakai path server. Jika di Local, abaikan.
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
