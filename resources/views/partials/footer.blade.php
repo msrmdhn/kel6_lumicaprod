d3058b89c64eebf25685c6369ac655e667d03ece
Sabda Ramadhan <ramadhansabda2@gmail.com>
2025-12-03 18:35:41 +0700

    Menghapus semua isi repository

--- resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-dark text-white text-center py-3 mt-auto">
-    <div class="container">
-        <p class="mb-0">&copy; {{ date('Y') }} Lumica Production. All Rights Reserved.</p>
-        <small class="text-white-50">Designed by Kelompok 6</small>
-    </div>
-</footer>
